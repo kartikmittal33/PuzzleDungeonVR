8f7852269371d48112d511e50432728d873a8f1d
kmittal33 <mittal38@purdue.edu>
2019-04-08 21:51:13 -0400

    throwable and recapturable ball

--- Assets/Script/BallTeleportScript.cs
+++ Assets/Script/BallTeleportScript.cs
@@ -5,39 +5,65 @@ using Valve.VR;
 
 public class BallTeleportScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public SteamVR_Action_Boolean trig;
-    public SteamVR_Input_Sources hand;
-    public GameObject controller;
+  
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(getTrigDown())
-        {
-            transform.position = controller.transform.position;
-            Debug.Log("here");
-        }
-        if (getTrig())
-        {
-            transform.position = controller.transform.position;
-            Debug.Log(" not here");
 
-        }
     }
 
-    public bool getTrigDown()
+
+
+
+    public GameObject prefab;
+    public Rigidbody attachPoint;
+
+    public SteamVR_Action_Boolean trig = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("InteractUI");
+
+    SteamVR_Behaviour_Pose trackedObj;
+    FixedJoint joint;
+
+    private void Awake()
     {
-        return trig.GetStateDown(hand);
-    }
-    public bool getTrig()
-    {
-        return trig.GetState(hand);
+        trackedObj = GetComponent<SteamVR_Behaviour_Pose>();
     }
 
+    private void FixedUpdate()
+    {
+        if (joint == null && trig.GetStateDown(trackedObj.inputSource))
+        {
+            prefab.transform.position = attachPoint.transform.position;
+
+            joint = prefab.AddComponent<FixedJoint>();
+            joint.connectedBody = attachPoint;
+        }
+        else if (joint != null && trig.GetStateUp(trackedObj.inputSource))
+        {
+            GameObject go = joint.gameObject;
+            Rigidbody rigidbody = go.GetComponent<Rigidbody>();
+            Object.DestroyImmediate(joint);
+            joint = null;
+
+            Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+            if (origin != null)
+            {
+                rigidbody.velocity = origin.TransformVector(trackedObj.GetVelocity());
+                rigidbody.angularVelocity = origin.TransformVector(trackedObj.GetAngularVelocity());
+            }
+            else
+            {
+                rigidbody.velocity = trackedObj.GetVelocity();
+                rigidbody.angularVelocity = trackedObj.GetAngularVelocity();
+            }
+
+            rigidbody.maxAngularVelocity = rigidbody.angularVelocity.magnitude;
+        }
+    }
+
+    
 
 }
