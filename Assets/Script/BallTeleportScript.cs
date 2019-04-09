267451bcd1c4e50829f62b4642a18facff0e2270
kmittal33 <mittal38@purdue.edu>
2019-04-08 20:34:44 -0400

    Ball Attached to controller

---
+++ Assets/Script/BallTeleportScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class BallTeleportScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SteamVR_Action_Boolean trig;
+    public SteamVR_Input_Sources hand;
+    public GameObject controller;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(getTrigDown())
+        {
+            transform.position = controller.transform.position;
+            Debug.Log("here");
+        }
+        if (getTrig())
+        {
+            transform.position = controller.transform.position;
+            Debug.Log(" not here");
+
+        }
+    }
+
+    public bool getTrigDown()
+    {
+        return trig.GetStateDown(hand);
+    }
+    public bool getTrig()
+    {
+        return trig.GetState(hand);
+    }
+
+
+}
