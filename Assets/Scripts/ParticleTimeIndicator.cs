ccdebc0dfa35756fb8270be7e8deca43eeb11b1d
Henry Poggie <henry.poggie@gmail.com>
2019-04-21 16:48:20 -0400

    Change particle color if we're slow motion

---
+++ Assets/Scripts/ParticleTimeIndicator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTimeIndicator : MonoBehaviour
+{
+    private void Update () {
+        GetComponent<ParticleSystem>().startColor = TimeManager.instance.slowMotion ? Color.red : Color.green;
+    }
+}
