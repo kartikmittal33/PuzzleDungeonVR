220b3320148e77a5ac835b611d9162036b239885
Henry Poggie <henry.poggie@gmail.com>
2019-04-21 11:54:52 -0400

    Don't launch while slow motion

--- Assets/Scripts/LaunchBall.cs
+++ Assets/Scripts/LaunchBall.cs
@@ -7,6 +7,7 @@ public class LaunchBall : MonoBehaviour
 {
 	void OnCollisionExit(Collision col)
 	{
+		if (TimeManager.instance.slowMotion) return;
 		var rb = col.gameObject.GetComponent<Rigidbody>();
 
 		rb.AddForce(transform.forward * GetComponent<MovingBoxScript1>().speed * 50);
