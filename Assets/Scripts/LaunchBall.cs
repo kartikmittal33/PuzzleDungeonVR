b71bae614501ca6a5220913f83d067ed75d84530
Henry Poggie <henry.poggie@gmail.com>
2019-04-21 11:42:42 -0400

    Launch ball in a bad and janky way

---
+++ Assets/Scripts/LaunchBall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MovingBoxScript1))]
+public class LaunchBall : MonoBehaviour
+{
+	void OnCollisionExit(Collision col)
+	{
+		var rb = col.gameObject.GetComponent<Rigidbody>();
+
+		rb.AddForce(transform.forward * GetComponent<MovingBoxScript1>().speed * 50);
+	}
+}
