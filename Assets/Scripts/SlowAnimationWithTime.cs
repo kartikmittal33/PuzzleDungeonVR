6d610a24c21c2ac349e59d5da92cef5ff5791604
Henry Poggie <henry.poggie@gmail.com>
2019-04-23 21:59:21 -0400

    Alter animation speed with time for target

---
+++ Assets/Scripts/SlowAnimationWithTime.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowAnimationWithTime : MonoBehaviour
+{
+    void Update()
+    {
+		GetComponent<Animator>().speed = TimeManager.instance.slowMotion ? 1.0f : 1.5f;
+    }
+}
