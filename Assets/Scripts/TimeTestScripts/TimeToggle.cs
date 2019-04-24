7208e7c66567d66f7f05a6a7fd3a602b72defa30
kmittal33 <mittal38@purdue.edu>
2019-04-23 22:30:03 -0400

    Added slomo sound and normal sound on book

--- Assets/Scripts/TimeTestScripts/TimeToggle.cs
+++ Assets/Scripts/TimeTestScripts/TimeToggle.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class TimeToggle : MonoBehaviour
 {
+    /// <summary>
+    /// https://freesound.org/people/n-lerche/sounds/459618/
+    /// https://retired.sounddogs.com/sound-effects/slow-motion-doppler-efx-965440
+    /// https://freesound.org/people/13GPanska_Lakota_Jan/sounds/378355/
+    /// </summary>
+    public AudioClip slowToNormal;
+    public AudioClip normalToSlow;
+
+    AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("TRIGGER TIME CHANGE");
@@ -12,12 +27,14 @@ public class TimeToggle : MonoBehaviour
             if(TimeManager.instance.slowMotion == true)
             {
                 TimeManager.instance.slowMotion = false;
-                TorchSoundScript.NormalMotion();
+                source.pitch = 1;
+                source.PlayOneShot(slowToNormal);
             }
             else
             {
                 TimeManager.instance.slowMotion = true;
-                TorchSoundScript.SlowMotion();
+                source.pitch = 2;
+                source.PlayOneShot(normalToSlow);
 
             }
         }
