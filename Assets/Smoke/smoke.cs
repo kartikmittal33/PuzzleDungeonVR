b991c541bb233683f64a8a0629a8a1aca17d982a
Shiv Paul <shivapaul98@gmail.com>
2019-04-24 20:07:39 -0400

    Made smoke togglable from time

---
+++ Assets/Smoke/smoke.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smoke : MonoBehaviour
+{
+    void Update()
+    {
+        if(TimeManager.instance.slowMotion)
+        {
+            GetComponent<Renderer>().enabled = true;
+        }
+        else
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
